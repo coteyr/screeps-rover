542314ba077b208204240a8870c2a28f4c63ad1d
Robert Cotey <coteyr@gmail.com>
2023-02-15 15:38:16 -0500

    Setup Linting and fix gitignore

--- src/lib/RoomLevels/1.js
+++ src/lib/RoomLevels/1.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 class RoomLevel1 {
   static run(room) {
-    console.log(room.name)
-    // Added a comment
+    console.log(`tarting Tick for room: ${room.name}`)
   }
 }
+
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+/* global RoomLevel1 */
+
 module.exports.loop = function() {
   _.forEach(Game.rooms, function(room){
     RoomLevel1.run(room)
