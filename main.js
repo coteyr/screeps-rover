a7b1bba2fad6d997f8eeb080008884c54dbfe89b
Robert Cotey <coteyr@gmail.com>
2023-02-15 15:36:59 -0500

    add git ignore

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 class RoomLevel1 {
   static run(room) {
-    console.log(room.name)
-    // Added a comment
+    console.log(`tarting Tick for room: ${room.name}`)
   }
 }
+
+/* global RoomLevel1 */
+
 module.exports.loop = function() {
   _.forEach(Game.rooms, function(room){
     RoomLevel1.run(room)
