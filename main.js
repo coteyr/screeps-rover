979466fd8a0f0309323e54d68a2490176e46a638
Robert Cotey <coteyr@gmail.com>
2023-02-15 13:23:56 -0500

    test 3

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-module.exports.loop = function() {
+module.exports = {
+	run(room) {
+		console.log(room.name)
+	}
+}module.exports.loop = function() {
   _.forEach(Game.rooms, function(room){
     RoomLevel1.run(room)
   })
