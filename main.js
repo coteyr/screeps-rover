71fe3cb469dd7bbe50ffdca3803b31fb64fcd504
Robert Cotey <coteyr@gmail.com>
2023-02-16 15:53:29 -0500

    limit bootstrap creeps to 5

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ class RoomLevel1 {
     let spawns = _.filter(Game.spawns, function(spawn) {
       return spawn.pos.roomName === room.name
     })
-    _.forEach(spawns, function(spawn) {
-      if(spawn.store[RESOURCE_ENERGY] >= 150 && !spawn.spawning) {
-        spawn.spawnCreep([WORK, MOVE, CARRY], `bootstrap-${room.name}-${Game.time}`)
-      }
-    })
     let creeps = _.filter(Game.creeps, function(creep) {
       return creep.my && creep.pos.roomName == room.name
     })
+    _.forEach(spawns, function(spawn) {
+      if(creeps.length < 5 && spawn.store[RESOURCE_ENERGY] >= 150 && !spawn.spawning) {
+        spawn.spawnCreep([WORK, MOVE, CARRY], `bootstrap-${room.name}-${Game.time}`)
+      }
+    })
+
     _.forEach(creeps, function(creep) {
       let screep = new Bootstrap(creep)
       screep.run()
