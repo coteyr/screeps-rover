01d0f73ebe04b297fbf492e091dadcadae2c85b9
Robert Cotey <coteyr@gmail.com>
2023-02-15 21:43:11 -0500

    sticky target step one

--- main.js
+++ main.js
@@ -30,17 +30,20 @@ class Bootstrap {
 
   run() {
     let creep = this.creep
-    let source = _.first(creep.room.find(FIND_SOURCES))
 
     if(creep.store.getFreeCapacity([RESOURCE_ENERGY]) > 0) {
-      if(creep.harvest(source) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(source)
+      if(creep.harvest(creep.target) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(creep.target)
       }
     } else {
       if(creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
         creep.moveTo(creep.room.controller)
       }
     }
+  }
+
+  get target() {
+    return _.first(this.creep.room.find(FIND_SOURCES))
   }
 }
 
